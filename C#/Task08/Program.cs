43c044a1216e0ccc97fee0aef06a962f06e8f650
Denis <denushka.ru@gmail.com>
2023-09-24 20:47:41 +0300

    rename

---
+++ C#/Task08/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите целое число больше 0: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int count = 0;
+    while (count <= number)
+    {
+        if (count % 2 == 0)
+        {
+            if (count > 0)
+            {
+                Console.Write($"{count} ");
+            }
+        }
+        count += 1;
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
