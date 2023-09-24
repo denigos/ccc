8415405d90c29bbf19d4ec1f603a9c5a8fc42844
Denis <denushka.ru@gmail.com>
2023-09-24 20:51:01 +0300

    rename

---
+++ C#/Task05/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое целое число больше 0: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 0)
+{
+    int count = -numberA;
+    while (count <= numberA)
+    {
+        Console.Write($"{count} ");
+        count += 1;
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
