3cc143e033f1de4efcfee301f6d30f6db95ba1e3
Denis <denushka.ru@gmail.com>
2023-09-24 19:53:17 +0300

    HomeTask 6

---
+++ C#/Task06/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numberA = number % 2;
+
+if (numberA == 0)
+{
+    Console.Write($"Введенное вами  число  {number}, является четным");
+}
+else
+{
+    Console.Write($"Введенное вами  число  {number}, не является четным");
+}
