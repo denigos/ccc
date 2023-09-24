b4eeb7a8f36ed6a21af65b136ecb455a77b018c9
Denis <denushka.ru@gmail.com>
2023-09-24 12:44:22 +0300

    sss

--- C#/Task02/Program.cs
+++ C#/Task02/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе  целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"{numberA} Больше {numberB}");
+}
+else
+{
+    Console.WriteLine($"{numberA} Меньше {numberB}");
+}
