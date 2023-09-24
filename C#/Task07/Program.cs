28ce8f186a11d5e87c3a1950f6e5ac0c4661cfed
Denis <denushka.ru@gmail.com>
2023-09-24 18:43:13 +0300

    LastDigit Finish

---
+++ C#/Task07/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите 3-х значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastDigit = number % 10;
+Console.Write($"Последняя цифра вашего числа - {lastDigit}");
