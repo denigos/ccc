243308e8768970ea511350fe728e92fbfce44694
Denis <denushka.ru@gmail.com>
2023-09-24 12:26:58 +0300

    square number

---
+++ C#/Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе  целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA * numberA == numberB)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число неявляется квадратом второго");
+}
+
+
+
