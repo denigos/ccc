d3e386a6b4e54de71b07c7be70fe898229980dee
Denis <denushka.ru@gmail.com>
2023-09-24 18:19:38 +0300

    d

--- C#/Task5/Program.cs
+++ C#/Task5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое целое число больше 0: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 0)
+{
+    int count = -numberA;
+    while (count <= numberA)
+    {
+        Console.Write($"{count} ");
+        count += 1;
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
