09bf13ae93b6011dc9ce03b6be1a999daff7fbc6
Denis <denushka.ru@gmail.com>
2023-09-24 19:20:56 +0300

    HomeWork 4

---
+++ C#/Task 4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе  целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье  целое число: ");
+int numberС = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (max<numberB)
+{
+    max = numberB;
+}
+if (max < numberС)
+{
+    max = numberС;
+    
+}
+Console.WriteLine($"Max number: {max}");
