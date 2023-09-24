83dc2b8a6fd2c3334a98f6c4b6dc77075899dbcf
Denis <denushka.ru@gmail.com>
2023-09-24 13:08:10 +0300

    Task3 finish

--- C#/Task03/Program.cs
+++ C#/Task03/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер дня недели");
+String day = Console.ReadLine();
+if (day == "1")
+{
+ Console.WriteLine("Понедельник");
+}
+else if (day == "2")
+{
+ Console.WriteLine("Вторник");
+}
+else if (day == "3")
+{
+ Console.WriteLine("Среда");
+}
+else if (day == "4")
+{
+ Console.WriteLine("Четверг");
+}
+else if (day == "5")
+{
+ Console.WriteLine("Пятница");
+}
+else if (day == "6")
+{
+ Console.WriteLine("Субота");
+}
+else if (day == "7")
+{
+ Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Неверные данные");
+}
