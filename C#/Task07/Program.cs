ce104343e90acf713f47dff7266aca121643b7da
Denis <denushka.ru@gmail.com>
2023-09-24 18:52:47 +0300

    update

--- C#/Task07/Program.cs
+++ C#/Task07/Program.cs
@@ -1,4 +1,10 @@
 ﻿Console.WriteLine("Введите 3-х значное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <=999)
+{
 int lastDigit = number % 10;
-Console.Write($"Последняя цифра вашего числа - {lastDigit}");
+Console.Write($"Последняя цифра вашего числа - {lastDigit}"); 
+}
+else Console.WriteLine("Некоректный ввод");
+
